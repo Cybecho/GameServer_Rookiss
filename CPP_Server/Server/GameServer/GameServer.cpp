d27fc76fad237b2234f5aee172af7bf3222664a3
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-07-26 18:44:13 +0900

    Atomic 타입

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -1,41 +1,36 @@
 ﻿#include "pch.h"
 #include "CorePch.h"
 #include <iostream>
-
+#include <atomic>
 #include <thread>
 
-void HelloThread()
+atomic<int32> sum =	0;
+
+void Add() 
 {
-	cout << "Hello Thread" << endl;
+	for (int32 i = 0; i < 1'000'000; i++)
+	{
+		//sum ++;				// 그냥 sum++ 하면 이게 int인지 atomic<int>인지 몰라서 애매하다.
+		sum.fetch_add(1);	// 그래서 atomic을 명시하기 위해 이렇게 해야한다.
+	}
 }
 
-void HelloThread2(int32 num)
+void Sub()
 {
-    cout << num << endl;
+	for (int32 i = 0; i < 1'000'000; i++)
+	{
+		//sum--;				// 그냥 sum-- 하면 이게 int인지 atomic<int>인지 몰라서 애매하다.
+		sum.fetch_sub(1);	// 그래서 atomic을 명시하기 위해 이렇게 해야한다.
+	}
 }
 
 int main()
 {
-    //! 스레드 생성 (컨테이너)
-    vector<thread> v; // 스레드 컨테이너
-    
-    //! 스레드에 함수를 넣어서 생성
-    for (int32 i = 0; i < 10; i++)
-	{
-		v.push_back(thread(HelloThread2, i)); // 스레드 생성
-	}
+	thread t1(Add);
+	thread t2(Sub);
 
-    //! 스레드를 생성했으니, 이제 join을 해줘야 한다.
-    for (int32 i = 0; i < 10; i++)
-    {
-        if(v[i].joinable()) // joinable은 스레드가 실행중인지 확인
-			v[i].join();
-    }
+	if(t1.joinable()) t1.join();
+	if(t2.joinable()) t2.join();
 
-    thread t(HelloThread2, 10); // 스레드 생성
-
-    if (t.joinable()) // joinable은 스레드가 실행중인지 확인
-        t.join();   
-    
-    cout << "Hello Main" << endl;
+	cout << sum << endl;
 }
