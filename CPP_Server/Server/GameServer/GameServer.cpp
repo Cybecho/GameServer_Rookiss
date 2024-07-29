e89d4518427e2cf6bc75043f8ccba3f5aaa5d9e0
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-07-29 18:53:05 +0900

    일차 3: Lock 구현 이론, SpinLock (38분)
    일차 3: Lock 구현 이론, SpinLock (38분)

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -4,32 +4,63 @@
 #include <atomic>
 #include <thread>
 #include <mutex>
-#include "AccountManager.h"
-#include "UserManager.h"
 
-void Func1()
+class SpinLock
 {
-	for (int32 i = 0; i < 1000; ++i)
+public:
+	void lock()
 	{
-		UserManager::Instance()->ProcessSave();
+		// CAS (Compare And Swap)
+		bool expected = false; // 현재 상태가 free 이다. (기대값)
+		bool desired = true;   // 현재 상태가 lock 이다.
+
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false; // 해당 함수는 &bool 로 받아오기 때문에, 다시 false 로 초기화 해줘야 한다.
+		}
+
+	};
+
+	void unlock()
+	{
+		_locked.store(false); // store() 함수는 std::atomic 클래스의 멤버 함수로, 원자적으로 값을 저장하는 역할을 합니다. 
+		// 이 코드에서 _locked.store(false);는 _locked 변수에 false 값을 원자적으로 저장합니다.
+	};
+private:
+	atomic<bool> _locked = false;
+};
+
+int32 sum = 0;
+mutex m;
+SpinLock spinLock;
+
+void Add() 
+{
+	for (int32 i = 0; i < 100'000; i++) 
+	{
+		lock_guard<SpinLock> guard(spinLock);
+		sum++;
 	}
 }
 
-void Func2()
+void Sub()
 {
-	for (int32 i = 0; i < 1000; ++i)
+	for (int32 i = 0; i < 100'000; i++)
 	{
-		AccountManager::Instance()->ProcessLogin();
+		lock_guard<SpinLock> guard(spinLock);
+		sum--;
 	}
 }
 
-int main()
-{
-	thread t1(Func1);
-	thread t2(Func2);
 
- 	t1.join();
+int main() 
+{
+
+	thread t1(Add);
+	thread t2(Sub);
+
+	t1.join();
 	t2.join();
 
-	cout << "Main Thread End" << endl;
-};
+	cout << sum << endl;
+}
