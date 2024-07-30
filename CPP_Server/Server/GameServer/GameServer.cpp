5257dc6899310cbccab4864ecdc4c7f97468117b
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-07-30 15:48:18 +0900

    EventLock 구현
    EventLock 구현

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -4,97 +4,59 @@
 #include <atomic>
 #include <thread>
 #include <mutex>
+#include <Windows.h>
 
-class SpinLock
-{
-public:
-	void lock()
-	{
-		// CAS (Compare And Swap)
-		bool expected = false; // 현재 상태가 free 이다. (기대값)
-		bool desired = true;   // 현재 상태가 lock 이다.
-
-		while (_locked.compare_exchange_strong(expected, desired) == false)
-		{
-			expected = false; // 해당 함수는 &bool 로 받아오기 때문에, 다시 false 로 초기화 해줘야 한다.
-		}
-
-	};
-
-	void unlock()
-	{
-		_locked.store(false); // store() 함수는 std::atomic 클래스의 멤버 함수로, 원자적으로 값을 저장하는 역할을 합니다. 
-		// 이 코드에서 _locked.store(false);는 _locked 변수에 false 값을 원자적으로 저장합니다.
-	};
-private:
-	atomic<bool> _locked = false;
-};
-
-class EventLock
-{
-public:
-	void lock()
-	{
-		// CAS (Compare And Swap)
-		bool expected = false; // 현재 상태가 free 이다. (기대값)
-		bool desired = true;   // 현재 상태가 lock 이다.
-
-		while (_locked.compare_exchange_strong(expected, desired) == false)
-		{
-			expected = false; // 해당 함수는 &bool 로 받아오기 때문에, 다시 false 로 초기화 해줘야 한다.
-
-			// 방법 1 : 실패하면 100ms 뒤에 CPU 점유권 포기
-			this_thread::sleep_for(std::chrono::microseconds(100));
-			// 방법 2 : 실패하면 0초 뒤에 바로 CPU점유권 포기
-			this_thread::sleep_for(0ms);
-			// 방법 3 : yield() 를 통해 현재 CPU점유권 양보하기
-			this_thread::yield();
-		}
-
-	};
-
-	void unlock()
-	{
-		_locked.store(false); // store() 함수는 std::atomic 클래스의 멤버 함수로, 원자적으로 값을 저장하는 역할을 합니다. 
-		// 이 코드에서 _locked.store(false);는 _locked 변수에 false 값을 원자적으로 저장합니다.
-	};
-private:
-	atomic<bool> _locked = false;
-};
-
-
-int32 sum = 0;
 mutex m;
-SpinLock spinLock;
-EventLock eventLock;
+queue<int32> q;
+HANDLE handle;
 
-void Add() 
+//! 데이터를 삽입하는 스레드 (생산자)
+void Producer()
 {
-	for (int32 i = 0; i < 100'000; i++) 
+	while (true)
 	{
-		lock_guard<EventLock> guard(eventLock);
-		sum++;
+		//~ 유니크 락은 클래스로 설계되어, 스코프를 벗어나면 자동으로 소멸자가 실행되며 unlock 해준다
+		{
+			unique_lock<mutex> guard(m);		// 공유 자원에 데이터 삽입을 위해 잠금
+			q.push(1000);								// 큐에 데이터 삽입
+		}
+		::SetEvent(handle);								// 데이터 삽입 후 event에 이벤트 발생
+		//! 커널 오브젝트의 상태를 Signal 상태로 변경하고 대기중인 스레드를 깨운다!
+
+		this_thread::sleep_for(1000ms);			//  0.1초 쉰 후 CPU 점유 포기 (lock free)
 	}
 }
 
-void Sub()
+//! 데이터를 꺼내쓰는 스레드 (소비자)
+void Consumer()
 {
-	for (int32 i = 0; i < 100'000; i++)
+	while (true)
 	{
-		lock_guard<EventLock> guard(eventLock);
-		sum--;
+		//! handle에서 신호가 올때까지
+		::WaitForSingleObject(handle, INFINITE);	// event가 발생할 때까지 대기
+
+		//~ unique_lock 소멸자 unlock을 위한 단순 스코프 영역
+		{
+			unique_lock<mutex> guard(m);		// 공유 자원에 데이터 삽입을 위해 잠금
+			if (q.empty() == false)					// 큐가 비어있지 않다면
+			{
+				int32 data = q.front();
+				q.pop();
+				cout << "Data: " << data << endl;
+			}
+		}
 	}
 }
 
-
-int main() 
+int main()
 {
+	handle = ::CreateEvent(NULL, FALSE, FALSE, NULL);
 
-	thread t1(Add);
-	thread t2(Sub);
+	thread t1(Producer);
+	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
 
-	cout << sum << endl;
-} 
+	::CloseHandle(handle); // 이벤트 해제
+}
