89d04b1ec00269aa9413523d3bb7b15d30d904a3
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-07-30 12:24:03 +0900

    Lock구현 - Event
    Lock구현 - Event

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -30,15 +30,46 @@ private:
 	atomic<bool> _locked = false;
 };
 
+class EventLock
+{
+public:
+	void lock()
+	{
+		// CAS (Compare And Swap)
+		bool expected = false; // 현재 상태가 free 이다. (기대값)
+		bool desired = true;   // 현재 상태가 lock 이다.
+
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false; // 해당 함수는 &bool 로 받아오기 때문에, 다시 false 로 초기화 해줘야 한다.
+
+			this_thread::sleep_for(std::chrono::microseconds(100)); // 실패하면 0.1ms 대기
+			this_thread::sleep_for(0ms); // 실패하면 바로 양보
+			this_thread::yield(); // 양보
+		}
+
+	};
+
+	void unlock()
+	{
+		_locked.store(false); // store() 함수는 std::atomic 클래스의 멤버 함수로, 원자적으로 값을 저장하는 역할을 합니다. 
+		// 이 코드에서 _locked.store(false);는 _locked 변수에 false 값을 원자적으로 저장합니다.
+	};
+private:
+	atomic<bool> _locked = false;
+};
+
+
 int32 sum = 0;
 mutex m;
 SpinLock spinLock;
+EventLock eventLock;
 
 void Add() 
 {
 	for (int32 i = 0; i < 100'000; i++) 
 	{
-		lock_guard<SpinLock> guard(spinLock);
+		lock_guard<EventLock> guard(eventLock);
 		sum++;
 	}
 }
@@ -47,7 +78,7 @@ void Sub()
 {
 	for (int32 i = 0; i < 100'000; i++)
 	{
-		lock_guard<SpinLock> guard(spinLock);
+		lock_guard<EventLock> guard(eventLock);
 		sum--;
 	}
 }
@@ -63,4 +94,4 @@ int main()
 	t2.join();
 
 	cout << sum << endl;
-}
+} 
