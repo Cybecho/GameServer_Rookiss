a81efabc376fc37498e37813c08b265763afd0c3
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-08-01 17:06:09 +0900

    Condition Variable
    Condition Variable

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -8,22 +8,25 @@
 
 mutex m;
 queue<int32> q;
-HANDLE handle;
+
+condition_variable cv; // CV는 커널 오브젝트가 아니다 (유저 오브젝트임)
 
 //! 데이터를 삽입하는 스레드 (생산자)
 void Producer()
 {
 	while (true)
 	{
-		//~ 유니크 락은 클래스로 설계되어, 스코프를 벗어나면 자동으로 소멸자가 실행되며 unlock 해준다
+		// 1) Lock을 걸고
+		// 2) 공유변수 데이터를 수정하고
+		// 3) Lock을 풀고
+		// 4) 조건 변수를 통해 다른 스레드가 Lock을 걸 수 있도록 해준다.
+
 		{
 			unique_lock<mutex> guard(m);		// 공유 자원에 데이터 삽입을 위해 잠금
 			q.push(1000);								// 큐에 데이터 삽입
 		}
-		::SetEvent(handle);								// 데이터 삽입 후 event에 이벤트 발생
-		//! 커널 오브젝트의 상태를 Signal 상태로 변경하고 대기중인 스레드를 깨운다!
-
-		this_thread::sleep_for(1000ms);			//  0.1초 쉰 후 CPU 점유 포기 (lock free)
+		this_thread::sleep_for(1s);		// 1초 대기
+		cv.notify_one(); // 대기중인 스레드중 하나의 스레드만 꺠운다.
 	}
 }
 
@@ -32,31 +35,28 @@ void Consumer()
 {
 	while (true)
 	{
-		//! handle에서 신호가 올때까지
-		::WaitForSingleObject(handle, INFINITE);	// event가 발생할 때까지 대기
+		unique_lock<mutex> lock(m);
+		cv.wait(lock, []() { return q.empty() == false; });		// 큐가 비어있지 않다면 깨운다.
+		// 1) Lock을 걸고
+		// 2) 조건 변수 확인
+		// -> 만족 O : 대기wait 을 빠져나와 아래 코드를 실행		(큐에 데이터가 있다는 소리)
+		// -> 만족 X : 대기wait 상태로 전환							(큐에 데이터가 없다는 소리)
 
-		//~ unique_lock 소멸자 unlock을 위한 단순 스코프 영역
+		// notify_one 할때 lock을 잡고 있는것이 아니기 때문에 wait에서 lock을 넘겨줘야한다.
 		{
-			unique_lock<mutex> guard(m);		// 공유 자원에 데이터 삽입을 위해 잠금
-			if (q.empty() == false)					// 큐가 비어있지 않다면
-			{
-				int32 data = q.front();
-				q.pop();
-				cout << "Data: " << data << endl;
-			}
+			int32 data = q.front();
+			q.pop();
+			cout << "Queue Size: " << q.size() << " Data: " << data << endl;
 		}
 	}
 }
 
 int main()
 {
-	handle = ::CreateEvent(NULL, FALSE, FALSE, NULL);
 
 	thread t1(Producer);
 	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
-
-	::CloseHandle(handle); // 이벤트 해제
 }
