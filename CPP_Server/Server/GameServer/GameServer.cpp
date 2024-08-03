0063b46fc43612723a49d6d3a9cc3f871dd20b0c
Cybecho_Proxy <42949995+Cybecho@users.noreply.github.com>
2024-08-03 15:49:30 +0900

    메모리모델 1
    메모리모델 1

--- CPP_Server/Server/GameServer/GameServer.cpp
+++ CPP_Server/Server/GameServer/GameServer.cpp
@@ -7,51 +7,76 @@
 #include <Windows.h>
 #include <future>
 
-int32 x = 0;
-int32 y = 0;
-int32 r1 = 0;
-int32 r2 = 0;
-
-volatile bool ready; // 상황을 체크하기 위해 불라틸 변수 설정
-
-void Thread_1()
-{
-	while (!ready); // ready가 true가 될 때까지 뺑뻉이 돌며 대기
-
-	y = 1;		// Store y (y값에 R value 덮어쓰기)
-	r1 = x;	// Load x (x값을 L value에 저장)
-}
-
-void Thread_2()
-{
-	while (!ready); // ready가 true가 될 때까지 뺑뻉이 돌며 대기
-
-	x = 1;
-	r2 = y;
-}
+atomic<bool> flag;
 
 int main()
 {
-	int32 count = 0;
+	flag = false;
+	flag.store(true, memory_order::memory_order_seq_cst); // flag = true
+	bool val = flag.load(memory_order::memory_order_seq_cst); //  val = true
 
-	while (true)
+	//! 상황을 가정해보자
+	//~ 1) 이전 flag값을 prev에 넣고, flag를 false로 바꾸고 싶다.
+	//~ 1-1) 시도 1
 	{
-		ready = false; //~ ready를 false로 초기화
-		count++;
+		flag.store(true); // flag = true
 
-		x = y = r1 = r2 = 0;
+		bool prev = flag;
+		flag.store(false);
+		/*
+		근데 만약에 flag값 prev에 옮기는 과정에서, 다른 스레드가 flag값을 가져가 바꾸면 어칼거임
+		prev는 true를 기대하고 가져갔겠지만, 다른 코어의 캐시에 flag는 이미 false로 바뀌어있을 수 있음
+		사실상 이미 false값으로 수정되었는데, 또 fallse로 바꾸려고하는거겠지?
+		어처피 지금은 true 아님 false 둘 중 하나라 상관없지만, 이게 정수나 실수라면,, 당연히 문제가 발생하겠지
+		데이터 가시성 문제가 존재할 수 밖에 없겠지?
+		*/
+		//? 즉, 현재 문제점은 원자적으로 한큐에 처리되지 않아 문제가 발생한다는것이다..
+	}
+	//~ 1-2) 시도 2
+	{
+		flag.store(true); // flag = true
 
-		thread t1(Thread_1);
-		thread t2(Thread_2);
-
-		ready = true; //~ ready를 true로 초기화 (스레드 함수는 내부에서 while을 탈출하겠지?)
-		t1.join();
-		t2.join();
-		cout << "r1: " << r1 << ", r2: " << r2 << endl;
-
-		if (r1 == 0 && r2 == 0)
-			break; // r1과 r2가 동시에 0이 되는 상황이 있다면 while을 빠져나가겠지?
+		bool prev = flag.exchange(false); // 값을 읽고 쓰는걸 한 사이클로 처리하는 exchange 함수
+	}
+	//~ 1-3) 시도 3 CAS 조건부 수정
+	{
+		bool expected = false;		// 초기 기대값
+		bool desired = true;			// 바꿀 값
+		flag.compare_exchange_strong(expected, desired, memory_order::memory_order_seq_cst);
 	}
 
-	cout << count << " 번 만에 탈출성공" << endl;
+	//~ Spurious Failue (가짜 실패 문제)
+	{
+		bool expected = false;		// 초기 기대값
+		bool desired = true;			// 바꿀 값
+
+		// compare_exchange_strong
+		//compare_exchange_strong은 코드를 무조건 완수함
+		if (flag == expected)
+		{
+			flag = desired;
+			return true;
+		}
+		else
+		{
+			expected = flag;
+			return false;
+		}
+
+		// compare_exchange_weak
+		// compare_exchange_weak은 실패할 수 있음
+		// 원래는 무조건 성공해야하는건데, 하드웨어적 예외로 코드가 실패할 수 있음
+		if (flag == expected)
+		{
+			//~ 하드웨어적인 이유로 중간에 실패할 수 있음
+			// if(특정 예외 상황)
+			flag = desired;
+			return true;
+		}
+		else
+		{
+			expected = flag;
+			return false;
+		}
+	}
 }
